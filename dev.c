95ab4ad4cf37d7c991e07cbb14e2f4d6bee26da4
Naveed Usman <naveedusman@MacBook-Air-de-Usman.local>
2025-01-22 12:21:56 +0100

    Création Jeu simple devinette en C

--- dev.c
+++ dev.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int number, guess, attempts = 0;
+    
+    // Seed the random number generator
+    srand(time(0));
+    
+    // Generate a random number between 1 and 100
+    number = rand() % 100 + 1;
+
+    printf("Welcome to the Guessing Game!\n");
+
+    do {
+        printf("Enter your guess (1-100): ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > number) {
+            printf("Too high! Try again.\n");
+        } else if (guess < number) {
+            printf("Too low! Try again.\n");
+        } else {
+            printf("Correct! You've guessed the number in %d attempts.\n", attempts);
+        }
+    } while (guess != number);
+
     return 0;
 }
