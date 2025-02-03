358b43715251d787de146b633f2e3c880e6fa00f
Naveed Usman <pak_man_60180@icloud.com>
2025-02-03 09:45:53 +0100

    change

--- morpion.c
+++ morpion.c
@@ -17,7 +17,7 @@ int main() {
     char joueurActuel = 'X';
     int jeuEnCours = 1;
 
-    printf("Bienvenue dans le jeudu Morpion !\n");
+    printf("Bienvenue dans le jeu du Morpion !\n");
 
     while (jeuEnCours) {
         afficherGrille(grille);
