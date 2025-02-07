9fdf31d5a5ad24123e4324c3d88b0d6fac9d9ef2
Naveed Usman <pak_man_60180@icloud.com>
2025-02-07 09:33:54 +0100

    Entrainement docker

--- docker-flask/app.py
+++ docker-flask/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Docker avec Flask ! 🚀"
+    return "Hello, Dockeravec Flask ! 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
