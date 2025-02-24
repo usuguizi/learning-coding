32326393a9c1253a2ab619e056445c68e2a2a5a4
Naveed Usman <pak_man_60180@icloud.com>
2025-02-24 09:30:41 +0100

    Change

--- docker-flask/pendu.c
+++ docker-flask/pendu.c
@@ -17,7 +17,7 @@ int main() {
     int wordLength;
 
     printf("=== Jeu du Pendu ===\n");
-    printf("Joueur 1,entrez unmot (max %d lettres) : ", MAX_WORD_LENGTH);
+    printf("Joueur 1,entrez un mot (max %d lettres) : ", MAX_WORD_LENGTH);
     scanf("%s", word);
     
     wordLength = strlen(word);
