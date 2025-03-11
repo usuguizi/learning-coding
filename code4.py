e7e0e871d1e26ab735af86ff9d3c79e3ba972720
Naveed Usman <pak_man_60180@icloud.com>
2025-03-11 11:11:40 +0100

    Change

--- code4.py
+++ code4.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
         print("1. Ajouter une tâche")
         print("2. Supprimer une tâche")
         print("3. Listerles tâches")
-        print("4. Marquer une tâche comme terminée")
+        print("4. Marquerune tâche comme terminée")
         print("5. Lister les tâches en retard")
         print("6. Quitter")
         print("============================================")
