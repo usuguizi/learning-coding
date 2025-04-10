1e524e42fcfc6c03c3f67c4d7ae8855d22c7d315
Naveed Usman <pak_man_60180@icloud.com>
2025-04-10 20:25:19 +0200

    change

--- docker-flask/pendu.c
+++ docker-flask/pendu.c
@@ -31,7 +31,7 @@ int main() {
 
     while (attemptsLeft > 0) {
         displayWord(word, revealed);
-        printf("\nTentativesrestantes : %d\n", attemptsLeft);
+        printf("\nTentatives restantes : %d\n", attemptsLeft);
         printf("Entrez une lettre : ");
         scanf(" %c", &guessedLetter);
         guessedLetter = tolower(guessedLetter);
